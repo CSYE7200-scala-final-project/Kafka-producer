68b27050fbe3775696046e845d43001974b28f2d
rohanchoudhary2509 <43220743+rohanchoudhary2509@users.noreply.github.com>
2020-04-13 21:36:08 -0400

    Create AccountTest.scala

---
+++ src/test/scala/edu/neu/coe/scala/AccountTest.scala
@@ -0,0 +1,12 @@
+import org.scalatest.Assertions._
+import org.scalatest.Account
+
+class AccountTest extends Account {
+
+assert(cb.getDebugEnabled==="true")
+assert(cb.getOAuthConsumerKey==="")
+assert(cb.getOAuthConsumerSecret==="")
+assert(cb.getOAuthAccessToken===""")
+assert(cb.getOAuthAccessTokenSecret===""")
+
+}
